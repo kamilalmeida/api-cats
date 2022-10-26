1c2a684570180190d353a4e4a2c030e511068cbd
Kamila Silva <kamila.silva@pmweb.com.br>
2022-10-26 13:05:53 -0300

    atualizando código

--- server.js
+++ server.js
@@ -1,96 +1,79 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 let cats = [];
-let id = 1
+let id = 1;
 
 app.use(express.json());
 
-//Rafael coding here is OK 
 app.post("/cat", function (req, res) {
+  const { name, colors } = req.body;
+  const cat = { id: id, name: name, colors: colors };
 
-    const { name, colors } = req.body;
-    const cat = { "id": id, "name": name, "colors": colors };
+  if (!name || !colors) return res.status(400).json();
 
-    if (!name || !colors) return res.status(400).json();
-
-    cats.push(cat);
-    id = id + 1;
-    return res.status(201).json(cat);
+  cats.push(cat);
+  id = id + 1;
+  return res.status(201).json(cat);
 });
 
-//Rafael coding is here OK
 app.get("/cat", function (req, res) {
-    if (cats.length <= 0) return res.status(200).json();
-    res.json(cats);
+  if (cats.length <= 0) return res.status(200).json();
+  res.json(cats);
 });
 
-//Rafael coding is here OK 
 app.get("/cat/:id", function (req, res) {
+  const { id } = req.params;
+  const cat = cats.find((cat) => cat.id == id);
 
-    const { id } = req.params;
-    const cat = cats.find(cat => cat.id == id);
+  if (!cat) return res.status(404).json();
 
-    if (!cat) return res.status(404).json();
-
-    res.json(cat);
+  res.json(cat);
 });
-//Rafael coding is here OK
+
 app.patch("/cat/:id", function (req, res) {
+  const { id } = req.params;
 
-    const { id } = req.params;
+  const { body } = req;
 
-    const { body } = req;
+  const cat = cats.find((cat) => cat.id == id);
+  console.log(cat);
 
-    const cat = cats.find(cat => cat.id == id);
-    console.log(cat)
+  let name = req.body.name;
 
-    let name = req.body.name
+  let colors = req.body.colors;
 
-    let colors = req.body.colors
+  cats[id - 1] = cat;
 
-    cats[id - 1] = cat
+  if (!cat) return res.status(404).json({ resposta: "cat not found" });
 
-    if (!cat) return res.status(404).json({ "resposta": "gato não encontrado" });
+  if (name != null) {
+    cat.name = name;
+  }
 
-    if (name != null) {
-     
-        cat.name = name
-    }
+  if (colors != null) {
+    cat.colors = colors;
+  }
 
-    if (colors != null) {
+  if (!name && !colors) return res.status(400).json();
 
-      cat.colors = colors
-
-    }
-
-    if (!name && !colors) return res.status(400).json();
-
-      res.json(cat)
+  res.json(cat);
 });
 
-// Rafael coding is here OK  
 app.delete("/cat/:id", function (req, res) {
-    
+  const { id } = req.params;
+  const cat = cats.find((cat) => cat.id == id);
+  if (cat) {
+    const catFiltrado = cats.filter((gato) => gato.id != id);
+    cats = catFiltrado;
 
-    
-        const  {id} = req.params
-        const cat = cats.find(cat => cat.id == id)
-        if (cat) {
-           const catFiltrado = cats.filter(gato => gato.id != id)
-           cats = catFiltrado
-            
-           console.log(catFiltrado)
-          return res.status(204).json({"resposta":"TUDO CERTO"})
-          console.log()
-        }
-    
-       if (!cat) return res.status(404).json({"resposta":"gato não encontrado"});
-        res.json(cat)
-    
-      
+    return res.status(204).json();
+  }
+
+  if (!cat) return res.status(404).json({ resposta: "cat not found" });
+  res.json(cat);
 });
 
-app.listen(3000, function () {
-    console.log("server is running");
+app.listen(3001, function () {
+  console.log("server is running");
 });
